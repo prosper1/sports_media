7e70c760bc84aae955f2e86f8b364eb69ba6b6d2
gundoprosper <gundotshili@gmail.com>
2021-01-03 04:39:18 +0200

    added prediction detail

--- sports-view/src/app/blog/prediction-detail/prediction-detail.component.ts
+++ sports-view/src/app/blog/prediction-detail/prediction-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PostsService } from 'src/app/_services/posts.service';
 
 @Component({
   selector: 'app-prediction-detail',
@@ -6,8 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./prediction-detail.component.css']
 })
 export class PredictionDetailComponent implements OnInit {
-
-  constructor() { }
+  sharedData = {
+    author: 2,
+    introduction: "hhdhdhdh",
+    team1: "Barcelona",
+    team2: "Madrid",
+    goals_halftime: "0:0",
+    goals_fulltime: "1:0",
+    conclusion: "jdjdjdjd",
+    match_date: "2021-01-02T21:21:32Z",
+    actual_score: "1:0"
+  };
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private postService: PostsService,
+  ) {
+    this.route.queryParams.subscribe(params => {
+      if (this.router.getCurrentNavigation().extras.state) {
+        const result = this.router.getCurrentNavigation().extras.state.post;
+        this.sharedData = result[0];
+      }
+      else {
+        const postId = this.route.snapshot.params.id;
+        this.postService.getPrediction(postId).subscribe(data => {
+          this.sharedData = data;
+        });
+      }
+    });
+   }
 
   ngOnInit(): void {
   }
