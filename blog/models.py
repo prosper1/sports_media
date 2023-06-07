09ec7e7de56645221b42c65b5c629c26eb3e7cd5
prosper1 <gundotshili@gmail.com>
2023-06-07 09:12:26 +0000

    migrate to django 4.0

--- blog/models.py
+++ blog/models.py
@@ -59,14 +59,14 @@ class Inserts(models.Model):
 
 class Genre(MPTTModel):
     name = models.TextField(max_length=50, unique=True)
-    parent = TreeForeignKey('self', null=True, blank=True, related_name='children', db_index=True, on_delete=True)
+    parent = TreeForeignKey('self', null=True, blank=True, related_name='children', db_index=True, on_delete=models.CASCADE)
 
     class MPTTMeta:
         order_insertion_by = ['name']
 
 class Post(models.Model):
 
-    author = models.ForeignKey('auth.User', on_delete=False)
+    author = models.ForeignKey('auth.User', on_delete=models.SET_NULL,null=True)
     title = models.CharField('header', max_length=200)
     discription = models.TextField()
     model_pic = models.ImageField(upload_to='pic_folder/', default = 'pic_folder/None/no-img.jpg', verbose_name='blogpics')
@@ -76,7 +76,7 @@ class Post(models.Model):
     subscription = models.ManyToManyField(SubscriptionPrices, related_name="subscriptions",related_query_name="subscrptions",verbose_name=u'subs',blank=True)
     published_date = models.DateTimeField(
             blank=True, null=True)
-    category = TreeForeignKey(Category, blank=True, null=True, related_name='catt', on_delete=False)
+    category = TreeForeignKey(Category, blank=True, null=True, related_name='catt', on_delete=models.SET_NULL)
     keywords = models.ManyToManyField(Keywords, related_name="keywordss", related_query_name="keywordss",
                                       verbose_name=u'tagss')
     is_review = models.BooleanField(default=False)
@@ -96,9 +96,9 @@ class Post(models.Model):
 
 
 class Comments(models.Model):
-    user = models.ForeignKey('auth.User', on_delete=False)
+    user = models.ForeignKey('auth.User', on_delete=models.SET_NULL,null=True)
     content = models.TextField()
-    post = models.ForeignKey(Post, on_delete=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
     created_date = models.DateTimeField(
             default=timezone.now)
 
@@ -111,18 +111,18 @@ class Contact(models.Model):
     content = models.TextField()
 
 class Profile(models.Model):
-    user = models.ForeignKey('auth.User', on_delete=False)
+    user = models.ForeignKey('auth.User', on_delete=models.SET_NULL,null=True)
     bio = models.CharField(max_length=140)
     website = models.URLField(max_length=250)
     work_link1 = models.URLField(max_length=250)
     work_link2 = models.URLField(max_length=250)
-    category = TreeForeignKey(Category, blank=True, null=True, related_name='categ', on_delete=False)
+    category = TreeForeignKey(Category, blank=True, null=True, related_name='categ', on_delete=models.SET_NULL)
     keywords = models.ManyToManyField(Keywords, related_name="kword", related_query_name="kwords",
                                       verbose_name=u'tgs')
 
 
 class Prediction(models.Model):
-    author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
+    author = models.ForeignKey('auth.User',on_delete=models.CASCADE,null=True)
     introduction = models.CharField(max_length=100)
     team1 = models.CharField(max_length=30)
     team2 = models.CharField(max_length=30)
